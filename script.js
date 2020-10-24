c1ea59f19a7d8a4b64e84e88fe1a1cd1da36b170
duho3abp <DanZhidkof@yandex.ru>
2020-10-24 16:46:35 +0300

    some change

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const pixelBackgroundLights = ({pixelsIdentify}) => {
     const pixels = document.querySelectorAll(pixelsIdentify);
 
     const animation = (pixel) => {
-        const color = Math.floor(Math.random() * 500 + 100);
+        const color = Math.floor(Math.random() * 200 + 100);
         pixel.style.background = `#${color}`;
 
         setTimeout(() => window.requestAnimationFrame( () => animation(pixel) ), 4000) ;
@@ -12,7 +12,7 @@ const pixelBackgroundLights = ({pixelsIdentify}) => {
 
     
     pixels.forEach(pixel => {
-        pixel.style.opacity = `0.7`;
+        pixel.style.opacity = `0.2`;
         window.requestAnimationFrame( () => animation(pixel) );
     });
 };
