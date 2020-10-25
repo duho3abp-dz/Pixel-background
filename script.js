49d918eaa3a229713a42377021f899f91a1281b6
duho3abp <DanZhidkof@yandex.ru>
2020-10-25 22:36:18 +0300

    clear code

--- script.js
+++ script.js
@@ -3,17 +3,16 @@
 const pixelBackgroundLights = ({pixelsIdentify}) => {
     const pixels = document.querySelectorAll(pixelsIdentify);
 
-    const animation = (pixel) => {
+    const animation = pixel => {
         const color = Math.floor(Math.random() * 200 + 100);
         pixel.style.background = `#${color}`;
 
         setTimeout(() => window.requestAnimationFrame( () => animation(pixel) ), 4000) ;
     }
 
-    
     pixels.forEach(pixel => {
         pixel.style.opacity = `0.2`;
-        window.requestAnimationFrame( () => animation(pixel) );
+        window.requestAnimationFrame(() => animation(pixel));
     });
 };
 
